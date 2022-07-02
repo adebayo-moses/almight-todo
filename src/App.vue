64725faa8f5674690ec84ba403043ab0d926aa96
Adebayo Moses <adebayomoses597@gmail.com>
2022-07-02 21:20:45 +0100

    update categories and functions

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,81 @@
 <script setup>
-import { ref, onMounted, computed, watch } from "vue";
+import { ref, onMounted, computed, watch } from 'vue';
+const todos = ref([]);
+const name = ref(' ');
+
+const input_content = ref('');
+const input_category = ref(null);
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal);
+});
+
+onMounded(() => {
+  name.value;
+});
 </script>
 
-<template>Hello World, i'm building my first ever todo app</template>
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up,
+        <input type="text" id="name" placeholder="name here" v-model="name" />
+      </h2>
+    </section>
+
+    <section class="create-todo">
+      <h3>Create a TODO</h3>
+
+      <form id="new-todo-form" @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="e.g. make a wish"
+          v-model="input_content"
+        />
+
+        <h4>Pick a category.</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category2"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+    </section>
+  </main>
+</template>
